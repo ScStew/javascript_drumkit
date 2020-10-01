7dfdf09907d2bb4d4251c53bc678bdaf461b5ce4
ScStew <scottmstewart2@gmail.com>
2020-09-30 21:06:26 -0400

    added key visuals with simple css

--- app.rb
+++ app.rb
@@ -2,5 +2,6 @@ require 'sinatra'
 load './local_env.rb' if File.exist?('./local_env.rb')
 
 get "/" do
-	erb :page
+    keys = ['A',"S","D","F","G","H","J","K","L"]
+	erb :page, locals:{keys:keys}
 end
